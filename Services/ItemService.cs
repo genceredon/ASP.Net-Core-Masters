2fee2071de6c2d5a684afc58f4b1a55d67b0f10b
genceredon <gjgceredon@gmail.com>
2021-02-20 21:41:43 +0800

    Revert "Revert "- initial commit for Homework#2""
    This reverts commit d96747fd13af693f7f037ba4d1a3c36220fd3639.

--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.DTO;
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Services
 {
     public class ItemService
     {
-        public IEnumerable<string> GetAll(int userId)
+        public int GetAll(int userId)
         {
-            return new string[] { $"Inside ItemService: value1 '{userId}'", "value2" };
+            return userId;
+        }
+
+        //public async Task<object> Save(ItemDTO text)
+        //{
+        //    return await Task.FromResult<object>($"Inside ItemService Save method '{text}'.");
+        //}
+
+        public string Save(ItemDTO text)
+        {
+            return $"Inside ItemService Save method '{text.Text}'.";
         }
     }
 }
